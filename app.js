1e022aa37ba2f361c899bff70b21032018664102
Jesse Clark <jjclark1982@gmail.com>
2025-09-27 14:05:45 -0700

    use single compound path for each cuttable layer

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-import { html, render, useState, Component } from 'https://esm.sh/htm/preact/standalone'
+import { html, render } from 'https://esm.sh/htm/preact/standalone'
+
+function roundRectPath({x, y, width, height, rx, ry}) {
+  return [
+      `M ${x + rx},${y}`,
+      `h ${width - 2*rx}`,
+      `c ${0.5*rx},0 ${rx},${0.5*ry} ${rx},${ry}`,
+      `v ${height - 2*ry}`,
+      `c 0,${0.5*rx} ${-0.5*rx},${ry} ${-rx},${ry}`,
+      `h ${-(width - 2*rx)}`,
+      `c ${-0.5*rx},0 ${-rx},${-0.5*ry} ${-rx},${-ry}`,
+      `v ${-(height - 2*ry)}`,
+      `c 0,${-0.5*ry} ${0.5*rx},${-ry} ${rx},${-ry}`,
+      `Z`,
+  ].join(' ');
+}
 
 function Sieve (props) {
   const defaultProps = {
@@ -11,54 +26,83 @@ function Sieve (props) {
   const {nRows, nCols, marginWidth, cellWidth, cellHeight} = Object.assign({}, defaultProps, props);
   const layerSpecs = [
     {factor: 1,  fill: 'white', stroke:'black', text: true},  // layer 1 is white with all numbers visible
-    {factor: 2,  fill: 'rgba(255, 0,   0,   0.25)'},  // layer 2 is clear with translucent red circles for multiples of 2 ( or cat-eye shapes )
-    {factor: 3,  fill: 'rgba(255, 255, 0,   0.33)'},  // layer 3 is clear with translucent yellow triangles for multiples of 3
-    {factor: 5,  fill: 'rgba(0,   0,   255, 0.25)'},  // layer 5 is clear with translucent blue pentagons for multiples of 5
-    {factor: 7,  fill: 'rgba(0,   255, 0,   0.25)'},  // layer 7 is clear with translucent green 7-stars for multiples of 7
-    {factor: 11, fill: 'rgba(255, 0,   255, 0.25)'},  // layer 11 is clear with translucent purple 11-stars for multiples of 11
-    {factor: 13, fill: 'rgba(0,   255, 255, 0.25)'},  // layer 13 is clear with translucent orange 13-stars for multiples of 13
+    {factor: 2,  fill: 'rgba(255, 0,   0,   0.25)'},  // layer 2 is clear with translucent red for multiples of 2 ( or cat-eye shapes )
+    {factor: 3,  fill: 'rgba(255, 255, 0,   0.33)'},  // layer 3 is clear with translucent yellow for multiples of 3
+    {factor: 5,  fill: 'rgba(0,   0,   255, 0.25)'},  // layer 5 is clear with translucent blue for multiples of 5
+    {factor: 7,  fill: 'rgba(0,   255, 0,   0.25)'},  // layer 7 is clear with translucent green for multiples of 7
+    {factor: 11, fill: 'rgba(255, 0,   255, 0.25)'},  // layer 11 is clear with translucent purple for multiples of 11
+    {factor: 13, fill: 'rgba(0,   255, 255, 0.25)'},  // layer 13 is clear with translucent orange for multiples of 13
+
   ];
   const layers = [];
   for (const layerSpec of layerSpecs) {
     const {factor} = layerSpec;
+    // solid frame
+    const cutOutPaths = [
+      roundRectPath({
+        x: 0, y: 0,
+        width: nCols*cellWidth + 2*marginWidth, 
+        height: nRows*cellHeight + 2*marginWidth,
+        rx: 4*marginWidth, ry: 4*marginWidth
+      })
+    ];
+    // etched outlines
     const cellEls = [];
+    const etchPaths = [];
+    // engraved numbers
     const textEls = [];
+
     for (let row = 0; row < nRows; row++) {
       for (let col = 0; col < nCols; col++) {
         let cellNum = (row * nCols) + col + 1;
-        if (cellNum % factor == 0) {
-          if (cellNum == factor && factor != 1) {
-            cellEls.push(html`<circle class="factor-base"
-              cx=${(col+0.5)*cellWidth}
-              cy=${(row+0.5)*cellHeight}
-              r=${cellWidth / 4}
-              opacity="0.5"
-            />`);
-          }
-          else {
-            cellEls.push(html`<rect class="factor-multiple"
-              x=${col*cellWidth + marginWidth}
-              y=${row*cellHeight + marginWidth}
-              width=${cellWidth - 2*marginWidth}
-              height=${cellHeight - 2*marginWidth}
-              rx=${2*marginWidth}
-              ry=${2*marginWidth}
-              // stroke=${layerSpec.stroke || ''}
-            />`);
-          }
+
+        const cellPath = roundRectPath({
+          x: col*cellWidth + 2*marginWidth,
+          y: row*cellHeight + 2*marginWidth,
+          width: cellWidth - 2*marginWidth,
+          height: cellHeight - 2*marginWidth,
+          rx: 3*marginWidth,
+          ry: 3*marginWidth
+        });
+        const insetCellPath = roundRectPath({
+          x: col*cellWidth + 4*marginWidth,
+          y: row*cellHeight + 4*marginWidth,
+          width: cellWidth - 6*marginWidth,
+          height: cellHeight - 6*marginWidth,
+          rx: 2*marginWidth,
+          ry: 2*marginWidth
+        });
+
+        if (cellNum == factor && factor != 1) {
+          // base factor has a smaller cutout
+          etchPaths.push(cellPath);
+          cutOutPaths.push(insetCellPath);
         }
+        else if (cellNum % factor == 0) {
+          // multiples have etched outlines
+          etchPaths.push(cellPath);
+        }
+        else {
+          // non-multiples are fully cut out
+          cutOutPaths.push(cellPath);
+        }
+
         if (layerSpec.text) {
           textEls.push(html`<text class="cell-number"
-            x=${(col+0.5)*cellWidth}
-            y=${(row+0.5)*cellHeight}
+            x=${(col+0.5)*cellWidth + marginWidth}
+            y=${(row+0.5)*cellHeight + marginWidth}
             dominant-baseline="central"
             text-anchor="middle"
           >${cellNum}</text>`);
         }
       }
     }
-    layers.push(html`<g class="factor-layer" id=${'factor-'+layerSpec.factor} fill=${layerSpec.fill}>
-      ${cellEls}
+
+    const cutOut = html`<path d=${cutOutPaths.join(' ')} fill-rule="evenodd" fill=${layerSpec.fill} />`;
+    const etch = html`<path d=${etchPaths.join(' ')} fill="transparent" />`; // stroke=${layerSpec.fill}
+    layers.push(html`<g class="factor-layer" id=${'factor-'+layerSpec.factor}>
+      ${cutOut}
+      ${etch}
       ${textEls}
     </g>`);
   }
@@ -77,10 +121,7 @@ function Sieve (props) {
           fill: #444;
         }
       </style>
-      <rect x=${0} y=${0} width=${nCols*cellWidth+2*marginWidth} height=${nRows*cellHeight+2*marginWidth} rx=${2*marginWidth} ry=${2*marginWidth} fill="#eee" />
-      <g transform="translate(${marginWidth}, ${marginWidth})">
-        ${layers}
-      </g>
+      ${layers}
     </svg>
   `;
 }
