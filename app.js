da6730b296459754910c8b198d246c2874f3111c
Jesse Clark <jjclark1982@gmail.com>
2025-09-29 21:29:58 -0700

    connect tab to frame seamlessly

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import { html, render, useState } from 'https://esm.sh/htm/preact/standalone'
 function roundRectPath({x, y, width, height, rx, ry}) {
   return [
     `M ${x + width - rx},${y}`,
-    // `h ${width - 2*rx}`,
     `c ${rx/2},0 ${rx},${ry/2} ${rx},${ry}`,
     `v ${height - 2*ry}`,
     `c 0,${rx/2} ${-rx/2},${ry} ${-rx},${ry}`,
@@ -46,14 +45,15 @@ function Sieve (props) {
     cellHeight: 72,
     layerSpecs: [
       {factor: 1,  fill: 'white', showOutlines: true, showNumbers: true},  // layer 1 is white with engraved labels
-      {factor: 2,  fill: 'rgba(255, 0,   0,   1.0)'},  // layer 2 is translucent red with cutouts for non-multiples of 2
-      {factor: 3,  fill: 'rgba(224, 224, 0,   1.0)'},  // layer 3 is translucent yellow with cutouts for non-multiples of 3
-      {factor: 5,  fill: 'rgba(0,   0,   255, 1.0)'},  // layer 5 is translucent blue with cutouts for non-multiples of 5
-      {factor: 7,  fill: 'rgba(0,   255, 0,   1.0)'},  // layer 7 is translucent green with cutouts for non-multiples of 7
-      {factor: 11, fill: 'rgba(255, 0,   255, 1.0)'},  // layer 11 is translucent purple with cutouts for non-multiples of 11
-      {factor: 13, fill: 'rgba(0,   255, 255, 1.0)'},  // layer 13 is translucent orange with cutouts for non-multiples of 13
-      {factor: 17, fill: 'rgba(0,   127, 255, 1.0)'},  // layer 17 is translucent orange with cutouts for non-multiples of 17
-      {factor: 19, fill: 'rgba(255, 127, 0,   1.0)'},  // layer 19 is translucent orange with cutouts for non-multiples of 19
+      {factor: 2,  fill: 'rgba(255, 0,   0,   0.75)'},  // layer 2 is translucent red with cutouts for non-multiples of 2
+      {factor: 3,  fill: 'rgba(255, 255, 0,   1.0)'},  // layer 3 is translucent yellow with cutouts for non-multiples of 3
+      {factor: 5,  fill: 'rgba(0,   0,   255, 0.75)'},  // layer 5 is translucent blue with cutouts for non-multiples of 5
+      {factor: 7,  fill: 'rgba(0,   255, 0,   0.75)'},  // layer 7 is translucent green with cutouts for non-multiples of 7
+      {factor: 11, fill: 'rgba(255, 0,   255, 0.75)'},  // layer 11 is translucent purple with cutouts for non-multiples of 11
+      {factor: 13, fill: 'rgba(0,   255, 255, 0.75)'},  // layer 13 is translucent orange with cutouts for non-multiples of 13
+      {factor: 17, fill: 'rgba(0,   127, 255, 0.75)'},  // layer 17 is translucent with cutouts for non-multiples of 17
+      {factor: 19, fill: 'rgba(255, 127, 0,   0.75)'},  // layer 19 is translucent with cutouts for non-multiples of 19
+      {factor: 23, fill: 'rgba(127, 0,   255, 0.75)'},  // layer 23 is translucent with cutouts for non-multiples of 23
     ]
   };
   const {nRows, nCols, marginWidth, cellWidth, cellHeight, layerSpecs} = Object.assign({}, defaultProps, props);
@@ -67,7 +67,7 @@ function Sieve (props) {
   const layers = [];
   for (const layerSpec of layerSpecs) {
     const {factor} = layerSpec;
-    if (factor > nCols) {
+    if (factor > Math.sqrt(nCols * nRows)) {
       continue;
     }
     // solid frame
@@ -114,10 +114,18 @@ function Sieve (props) {
           textEls.push(html`<text class="legend-text"
             x=${(col+0.5)*cellWidth + marginWidth}
             y=${(row+0.5)*cellHeight + marginWidth + cellHeight/9}
+            text-anchor="middle"
           >${cellNum}</text>`);
         }
       }
     }
+
+    const outerFrame = roundRectPath({
+      x: 0, y: 0,
+      width: nCols*cellWidth + 2*marginWidth, 
+      height: nRows*cellHeight + 2*marginWidth,
+      rx: 4*marginWidth, ry: 4*marginWidth
+    });
 
     if (factor > 1 && factor <= nCols) {
       cutOutPaths.push(tabPath({
@@ -127,23 +135,20 @@ function Sieve (props) {
         height: cellHeight/2 + marginWidth,
         rx: 3*marginWidth,
         ry: 3*marginWidth
-      }));
+      }) + outerFrame.replace('M', 'L'));
       textEls.push(html`<text class="legend-text"
         x=${(factor-0.5)*cellWidth + marginWidth}
         y=${-cellHeight/6}
+        text-anchor="middle"
       >${factor}</text>`);
     }
+    else {
+      cutOutPaths.push(outerFrame);
+    }
 
-    cutOutPaths.push(roundRectPath({
-      x: 0, y: 0,
-      width: nCols*cellWidth + 2*marginWidth, 
-      height: nRows*cellHeight + 2*marginWidth,
-      rx: 4*marginWidth, ry: 4*marginWidth
-    }));
-
-    const cutOut = html`<path class="cutout-path" d=${cutOutPaths.join(' ')} fill-rule="evenodd" fill=${layerSpec.fill} opacity=${factor == 1 ? 1 : 0.25} style="mix-blend-mode:darken;" />`;
+    const cutOut = html`<path class="cutout-path" d=${cutOutPaths.join(' ')} fill-rule="evenodd" fill=${layerSpec.fill} opacity=${factor == 1 ? 1 : 1/3} style="mix-blend-mode:darken;" />`;
     const outline = layerSpec.showOutlines ? html`<path class="etch-path" d=${outlinePaths.join(' ')} fill="rgba(255,255,255,0)" />` : null; // stroke=${layerSpec.fill}
-    const legend = html`<g class="legend-text" fill="#444" text-anchor="middle" style="font-size: ${cellHeight/3}px; font-weight: bold; font-family: sans-serif;">${textEls}</g>`
+    const legend = html`<g class="legend-text" fill="#444" style="font-size: ${cellHeight/3}px; font-weight: bold; font-family: sans-serif;">${textEls}</g>`
     layers.push(html`<g class="factor-layer" id=${'factor-'+layerSpec.factor} transform=${`translate(0,${cellHeight/2+marginWidth})`}>
       ${cutOut}
       ${outline}
@@ -162,24 +167,27 @@ function Sieve (props) {
   `;
 }
 
-function App (props = {}) {
+function App() {
   const [nRows, setNRows] = useState(12);
   const [nCols, setNCols] = useState(12);
   const [gridSize, setGridSize] = useState(80);
   const [marginWidth, setMarginWidth] = useState(5);
+  const totalWidth = nCols*gridSize + 2*marginWidth;
+  const totalHeight = (nRows+0.5)*gridSize + 3*marginWidth;
   return html`
     <h1><a href="https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes">Sieve of Eratosthenes</a> Cutout Pattern Generator</h1>
     <p>
-      <abbr title=${((nCols*gridSize + 2*marginWidth)/96).toFixed(2) + " × " + (((nRows+0.5)*gridSize + 3*marginWidth)/96).toFixed(2) + " in\n" + ((nCols*gridSize + 2*marginWidth)/96*2.54).toFixed(2) + " × " + (((nRows+0.5)*gridSize + 3*marginWidth)/96*2.54).toFixed(2) + " cm"}>
-        Table Size:
-      </abbr>
+      Table Size:
       ${" "}
       <input type="number" style="width:3.5em;" name="nCols" value=${nCols} onChange=${function(event){setNCols(event.target.valueAsNumber)}} />
       ${" × "}
       <input type="number" style="width:3.5em;" name="nRows" value=${nRows} onChange=${function(event){setNRows(event.target.valueAsNumber)}} />
       ${" "}
-      <label>Grid Size (px): <input type="number" style="width:3.5em;" name="gridSize" value=${gridSize} onChange=${function(event){setGridSize(event.target.valueAsNumber)}} /></label>
-      ${" "}
+      <label>
+        <abbr title=${`Total Size\n${totalWidth} × ${totalHeight} px\n${(totalWidth/96).toFixed(2)} × ${(totalHeight/96).toFixed(2)} in\n${(totalWidth/96*2.54).toFixed(2)} × ${(totalHeight/96*2.54).toFixed(2)} cm`}>
+          Grid Size (px): <input type="number" style="width:3.5em;" name="gridSize" value=${gridSize} onChange=${function(event){setGridSize(event.target.valueAsNumber)}} />
+        </abbr>
+      </label>
       ${" "}
       <label>Margin (px): <input type="number" style="width:3.5em;" name="marginWidth" value=${marginWidth} onChange=${function(event){setMarginWidth(event.target.valueAsNumber)}} /></label>
       ${" "}
