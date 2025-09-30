9df7b0b1f197d5431c9afb0abd8624cf4c1bb707
Jesse Clark <jjclark1982@gmail.com>
2025-09-29 17:24:24 -0700

    add tabs to cutout path

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ import { html, render, useState } from 'https://esm.sh/htm/preact/standalone'
 
 function roundRectPath({x, y, width, height, rx, ry}) {
   return [
-      `M ${x + rx},${y}`,
-      `h ${width - 2*rx}`,
-      `c ${rx/2},0 ${rx},${ry/2} ${rx},${ry}`,
-      `v ${height - 2*ry}`,
-      `c 0,${rx/2} ${-rx/2},${ry} ${-rx},${ry}`,
-      `h ${-(width - 2*rx)}`,
-      `c ${-rx/2},0 ${-rx},${-ry/2} ${-rx},${-ry}`,
-      `v ${-(height - 2*ry)}`,
-      `c 0,${-ry/2} ${rx/2},${-ry} ${rx},${-ry}`,
-      `Z`,
+    `M ${x + width - rx},${y}`,
+    // `h ${width - 2*rx}`,
+    `c ${rx/2},0 ${rx},${ry/2} ${rx},${ry}`,
+    `v ${height - 2*ry}`,
+    `c 0,${rx/2} ${-rx/2},${ry} ${-rx},${ry}`,
+    `h ${-(width - 2*rx)}`,
+    `c ${-rx/2},0 ${-rx},${-ry/2} ${-rx},${-ry}`,
+    `v ${-(height - 2*ry)}`,
+    `c 0,${-ry/2} ${rx/2},${-ry} ${rx},${-ry}`,
+    `Z`,
+  ].join(' ');
+}
+
+function tabPath({x, y, width, height, rx, ry}) {
+  return [
+    `M ${x},${y}`,
+    `c ${rx/2},0 ${rx},${-ry/2} ${rx},${-ry}`,
+    `v ${-(height - 2*ry)}`,
+    `c 0,${-ry/2} ${rx/2},${-ry} ${rx},${-ry}`,
+    `h ${width - 2*ry}`,
+    `c ${rx/2},0 ${rx},${ry/2} ${rx},${ry}`,
+    `v ${height - 2*ry}`,
+    `c 0,${ry/2} ${rx/2},${ry} ${rx},${ry}`,
   ].join(' ');
 }
 
@@ -32,7 +45,7 @@ function Sieve (props) {
     cellWidth: 72,
     cellHeight: 72,
     layerSpecs: [
-      {factor: 1,  fill: 'white', showOutlines: true},  // layer 1 is white with engraved labels
+      {factor: 1,  fill: 'white', showOutlines: true, showNumbers: true},  // layer 1 is white with engraved labels
       {factor: 2,  fill: 'rgba(255, 0,   0,   1.0)'},  // layer 2 is translucent red with cutouts for non-multiples of 2
       {factor: 3,  fill: 'rgba(224, 224, 0,   1.0)'},  // layer 3 is translucent yellow with cutouts for non-multiples of 3
       {factor: 5,  fill: 'rgba(0,   0,   255, 1.0)'},  // layer 5 is translucent blue with cutouts for non-multiples of 5
@@ -40,7 +53,7 @@ function Sieve (props) {
       {factor: 11, fill: 'rgba(255, 0,   255, 1.0)'},  // layer 11 is translucent purple with cutouts for non-multiples of 11
       {factor: 13, fill: 'rgba(0,   255, 255, 1.0)'},  // layer 13 is translucent orange with cutouts for non-multiples of 13
       {factor: 17, fill: 'rgba(0,   127, 255, 1.0)'},  // layer 17 is translucent orange with cutouts for non-multiples of 17
-      // {factor: 19, fill: 'rgba(255, 127, 0,   1.0)'},  // layer 19 is translucent orange with cutouts for non-multiples of 19
+      {factor: 19, fill: 'rgba(255, 127, 0,   1.0)'},  // layer 19 is translucent orange with cutouts for non-multiples of 19
     ]
   };
   const {nRows, nCols, marginWidth, cellWidth, cellHeight, layerSpecs} = Object.assign({}, defaultProps, props);
@@ -54,10 +67,15 @@ function Sieve (props) {
   const layers = [];
   for (const layerSpec of layerSpecs) {
     const {factor} = layerSpec;
+    if (factor > nCols) {
+      continue;
+    }
     // solid frame
     const cutOutPaths = [];
     // etched outlines
     const outlinePaths = [];
+    // engraved numbers
+    const textEls = [];
 
     for (let row = 0; row < nRows; row++) {
       for (let col = 0; col < nCols; col++) {
@@ -92,11 +110,28 @@ function Sieve (props) {
           // multiples have etched outlines
           outlinePaths.push(cellPath);
         }
-
-        // if (layerSpec.showOutlines) {
-        //   outlinePaths.push(cellPath);
-        // }
+        if (layerSpec.showNumbers) {
+          textEls.push(html`<text class="legend-text"
+            x=${(col+0.5)*cellWidth + marginWidth}
+            y=${(row+0.5)*cellHeight + marginWidth}
+          >${cellNum}</text>`);
+        }
       }
+    }
+
+    if (factor > 1 && factor <= nCols) {
+      cutOutPaths.push(tabPath({
+        x: (factor-1)*cellWidth - 2*marginWidth,
+        y: 0,
+        width: cellWidth,
+        height: cellHeight/2 + marginWidth,
+        rx: 3*marginWidth,
+        ry: 3*marginWidth
+      }));
+      textEls.push(html`<text class="legend-text"
+        x=${(factor-0.5)*cellWidth + marginWidth}
+        y=${-cellHeight/4}
+      >${factor}</text>`);
     }
 
     cutOutPaths.push(roundRectPath({
@@ -106,36 +141,20 @@ function Sieve (props) {
       rx: 4*marginWidth, ry: 4*marginWidth
     }));
 
-    const cutOut = html`<path d=${cutOutPaths.join(' ')} fill-rule="evenodd" fill=${layerSpec.fill} opacity=${factor == 1 ? 1 : 0.25} style="mix-blend-mode:darken;" />`;
-    const outline = layerSpec.showOutlines ? html`<path d=${outlinePaths.join(' ')} fill="rgba(255,255,255,0)" />` : null; // stroke=${layerSpec.fill}
-    layers.push(html`<g class="factor-layer" id=${'factor-'+layerSpec.factor}>
+    const cutOut = html`<path class="cutout-path" d=${cutOutPaths.join(' ')} fill-rule="evenodd" fill=${layerSpec.fill} opacity=${factor == 1 ? 1 : 0.25} style="mix-blend-mode:darken;" />`;
+    const outline = layerSpec.showOutlines ? html`<path class="etch-path" d=${outlinePaths.join(' ')} fill="rgba(255,255,255,0)" />` : null; // stroke=${layerSpec.fill}
+    const legend = html`<g class="legend-text" fill="#444" dominant-baseline="central" text-anchor="middle" style="font-size: ${cellHeight/3}px; font-weight: bold; font-family: sans-serif;">${textEls}</g>`
+    layers.push(html`<g class="factor-layer" id=${'factor-'+layerSpec.factor} transform=${`translate(0,${cellHeight/2+marginWidth})`}>
       ${cutOut}
       ${outline}
+      ${legend}
     </g>`);
   }
 
-  // legend with engraved numbers
-  const textEls = [];
-  for (let row = 0; row < nRows; row++) {
-    for (let col = 0; col < nCols; col++) {
-      const cellNum = (row * nCols) + col + 1;
-        textEls.push(html`<text
-          x=${(col+0.5)*cellWidth + marginWidth}
-          y=${(row+0.5)*cellHeight + marginWidth}
-          dominant-baseline="central" text-anchor="middle"
-        >${cellNum}</text>`);
-    }
-  }
-  layers.push(html`<g class="legend-layer" id="legend"
-      fill="#444"
-      style="font-size: ${cellHeight/3}px; font-weight: bold; font-family: sans-serif;"
-    >
-    ${textEls}
-  </g>`);
   return html`
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
-      viewbox="0 0 ${nCols*cellWidth + 2*marginWidth} ${nRows*cellHeight + 2*marginWidth}"
-      width=${nCols*cellWidth + 2*marginWidth} height=${nRows*cellHeight + 2*marginWidth}
+      viewbox="0 0 ${nCols*cellWidth + 2*marginWidth} ${nRows*cellHeight + 2*marginWidth + cellHeight}"
+      width=${nCols*cellWidth + 2*marginWidth} height=${nRows*cellHeight + 2*marginWidth + cellHeight}
     >
       <title>Sieve ${nCols}x${nRows} ${cellWidth}+${marginWidth}pt</title>
       ${layers}
@@ -152,14 +171,14 @@ function App (props = {}) {
     <h1><a href="https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes">Sieve of Eratosthenes</a> Cutout Pattern Generator</h1>
     <p>
       Table Size:${" "}
-      <input type="number" size="3" name="nCols" value=${nCols} onChange=${function(event){setNCols(event.target.valueAsNumber)}} />
+      <input type="number" style="width:3.5em;" name="nCols" value=${nCols} onChange=${function(event){setNCols(event.target.valueAsNumber)}} />
       ${" × "}
-      <input type="number" size="3" name="nRows" value=${nRows} onChange=${function(event){setNRows(event.target.valueAsNumber)}} />
+      <input type="number" style="width:3.5em;" name="nRows" value=${nRows} onChange=${function(event){setNRows(event.target.valueAsNumber)}} />
       ${" "}
-      <label>Grid Size (pt): <input type="number" size="3" name="gridSize" value=${gridSize} onChange=${function(event){setGridSize(event.target.valueAsNumber)}} /></label>
+      <label>Grid Size (pt): <input type="number" style="width:3.5em;" name="gridSize" value=${gridSize} onChange=${function(event){setGridSize(event.target.valueAsNumber)}} /></label>
       ${" "}
       ${" "}
-      <label>Margin (pt): <input type="number" size="3" name="marginWidth" value=${marginWidth} onChange=${function(event){setMarginWidth(event.target.valueAsNumber)}} /></label>
+      <label>Margin (pt): <input type="number" style="width:3.5em;" name="marginWidth" value=${marginWidth} onChange=${function(event){setMarginWidth(event.target.valueAsNumber)}} /></label>
       ${" "}
       <button onClick=${downloadSVG}>Download SVG</button>
     </p>
